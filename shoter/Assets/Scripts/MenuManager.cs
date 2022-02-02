afb8b16f449022debb9ec4969220168cba74744a
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2022-02-02 19:44:28 +0100

    multi

---
+++ shoter/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MenuManager : MonoBehaviourPunCallbacks
+{
+    public static MenuManager Instance;
+    public MenuItem[] menuItems;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void SwitchMenu(string _name)
+    {
+        foreach (MenuItem mi in menuItems)
+        {
+            if (mi.menuItem == _name)
+                mi.gameObject.SetActive(true);
+            else
+                mi.gameObject.SetActive(false);
+        }
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
