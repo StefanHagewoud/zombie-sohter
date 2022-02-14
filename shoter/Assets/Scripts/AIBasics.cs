fcd808b21a514c357c6bebacaa1002059e62cfff
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2022-02-14 12:02:03 +0100

    Basic AI targeting/movement

--- shoter/Assets/Scripts/AIBasics.cs
+++ shoter/Assets/Scripts/AIBasics.cs
@@ -5,15 +5,56 @@ using UnityEngine.AI;
 
 public class AIBasics : MonoBehaviour
 {
-    NavMeshAgent nav;
+    [HideInInspector]public NavMeshAgent nav;
+    AIManager _AIManager;
 
+
+    //Targeting
+    public float targetRange;
+    public GameObject[] players;
+    [HideInInspector]public Transform targetDestination;
     void Start()
     {
+        _AIManager = FindObjectOfType<AIManager>();
+        players = _AIManager.players;
         nav = GetComponent<NavMeshAgent>();
+        GetClosestPlayer();
+        UpdateTarget();
     }
 
     public void UpdateTarget()
     {
+        GetClosestPlayer();
+        nav.destination = targetDestination.transform.position;
 
+        if (Vector3.Distance(gameObject.transform.position, targetDestination.transform.position) < targetRange)
+        {
+            nav.speed = 0;
+            gameObject.transform.LookAt(targetDestination);
+        }
+    }
+
+    public void GetClosestPlayer()
+    {
+        GameObject currentClosest = null;
+        float currentClosestDistance = 0;
+        foreach (GameObject player in players)
+        {
+            if (currentClosest == null)
+            {
+                currentClosest = player;
+                currentClosestDistance = Vector3.Distance(transform.position, currentClosest.transform.position);
+            }
+            else
+            {
+                float newDistance = Vector3.Distance(transform.position, currentClosest.transform.position);
+                if (newDistance < currentClosestDistance)
+                {
+                    currentClosest = player;
+                    currentClosestDistance = newDistance;
+                }
+            }
+        }
+        targetDestination = currentClosest.transform;
     }
 }
