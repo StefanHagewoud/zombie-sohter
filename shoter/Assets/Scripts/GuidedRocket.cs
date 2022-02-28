5bdf20d0d9888593865561077d035ff42a2c4e88
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2022-03-01 00:52:45 +0100

    rocket update

--- shoter/Assets/Scripts/GuidedRocket.cs
+++ shoter/Assets/Scripts/GuidedRocket.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class GuidedRocket : MonoBehaviour
 {
     public float speed;
-    public GameObject target;
 
+
+    [HideInInspector]public GameObject[] players;
+    public GameObject target;
 
 
     void Start()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject currentClosest = null;
+        float currentClosestDistance = 0;
+        foreach (GameObject player in players)
+        {
+            if (currentClosest == null)
+            {
+                currentClosest = player;
+                currentClosestDistance = Vector3.Distance(transform.position, currentClosest.transform.position);
+            }
+            else
+            {
+                float newDistance = Vector3.Distance(transform.position, currentClosest.transform.position);
+                if (newDistance < currentClosestDistance)
+                {
+                    currentClosest = player;
+                    currentClosestDistance = newDistance;
+                }
+            }
+        }
+        target = currentClosest;
     }
 
     void Update()
