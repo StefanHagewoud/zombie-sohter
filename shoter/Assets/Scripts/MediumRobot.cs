f4077e011f599cd61ba3e2b10ff98e6c952b5e04
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2022-03-30 23:53:42 +0200

    medium robot shoot

--- shoter/Assets/Scripts/MediumRobot.cs
+++ shoter/Assets/Scripts/MediumRobot.cs
@@ -10,24 +10,13 @@ public class MediumRobot : AIBasics
     public float AttackReloadTime;
     bool reloading;
 
-    public Transform bulletSpawn;
+    public Transform[] bulletSpawns;
 
     [Header("Audio")]
     public AudioClip footstep;
     public override void Update()
     {
         base.Update();
-
-        if (!reloading)
-        {
-            nav.speed = moveSpeed;
-            if (Vector3.Distance(targetDestination.position, transform.position) <= nav.stoppingDistance)
-            {
-                nav.speed = 0;
-
-                FireMainWeapon();
-            }
-        }
 
         if (Vector3.Distance(targetDestination.position, transform.position) <= nav.stoppingDistance)
         {
@@ -36,31 +25,34 @@ public class MediumRobot : AIBasics
             lookPos.y = 0;
             var rotation = Quaternion.LookRotation(lookPos);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+            anim.SetLayerWeight(anim.GetLayerIndex("Shooting"), 1);
         }
         else
         {
             anim.SetFloat("Blend", 0f, 0.1f, Time.deltaTime);
+            anim.SetLayerWeight(anim.GetLayerIndex("Shooting"), 0);
         }
     }
 
-    public void FireMainWeapon()
+    public void Shoot()
     {
-        StartCoroutine(Shooting());
-        IEnumerator Shooting()
+        RaycastHit Hit;
+        foreach(Transform bulletSpawn in bulletSpawns)
         {
-            anim.SetLayerWeight(anim.GetLayerIndex("Shooting"), 1);
-            yield return new WaitForSecondsRealtime(2f);
-            anim.SetLayerWeight(anim.GetLayerIndex("Shooting"), 0);
-            nav.speed = moveSpeed;
-            StartCoroutine(Reloading());
-        }
-        IEnumerator Reloading()
-        {
-            reloading = true;
-            yield return new WaitForSecondsRealtime(AttackReloadTime);
-            reloading = false;
+            bulletSpawn.GetComponent<ParticleSystem>().Play();
+
+            if (Physics.Raycast(bulletSpawn.transform.position, targetDestination.position, out Hit, 10f))
+            {
+                Debug.Log(Hit.collider.name);
+
+                if (Hit.collider.CompareTag("Player"))
+                {
+                    Hit.collider.GetComponent<Health>().GetHit(damage);
+                }
+            }
         }
     }
+
 
     //SOUNDS
 
